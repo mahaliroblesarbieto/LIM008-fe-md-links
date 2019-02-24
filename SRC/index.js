d2ad4726585a9430cad7b732e0324fc66d769bb5
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-02-24 17:40:23 -0500

    uniendo funciones de links.js en index.js

--- SRC/index.js
+++ SRC/index.js
@@ -1,8 +1,22 @@
-import {evaluatePath, recognizeIfIsFile} from './Models/links.js';
+import {evaluatePath, recognizeIfIsFile, getFiles, getMDContent} from './Models/links.js';
 const mdLinks = (path, options) => {
     if (evaluatePath(path) === true){
-        recognizeIfIsFile(path)
+        if(recognizeIfIsFile(path) === false){
+            getFiles(path)
+        }else {
+            let extension = path.extname(path);
+            if(extension === '.md'){
+                getMDContent(path)
+            }
+
+        }
     }else{
-        transformToAbsPath(path)
+        const newPath = transformToAbsPath(path);
+        if(recognizeIfIsFile(newPath) === false){
+            getFiles(path)
+        }else{
+            let extension = path.extname(path);
+
+        }
     }
 }

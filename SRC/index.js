e18a5d4b7adab4b0df295cabd9cca38fb0097f67
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-02-26 02:05:38 -0500

    tratando de implementar función mdLinks/index.js

--- SRC/index.js
+++ SRC/index.js
@@ -1,20 +1,28 @@
-import {evaluatePath, recognizeIfIsFile, getFiles, getMDContent} from './Models/links.js';
+import {evaluatePath, recognizeIfIsFile, getFiles, getMDContent, convertMDToHtml, extractATagAttr, createArrLinkObj} from './Models/links.js';
+const paths = require('path');
 const mdLinks = (path, options) => {
+  let pathAbsolute;
+  let arrLink;
   if (evaluatePath(path) === true) {
-    if (recognizeIfIsFile(path) === false) {
-      getFiles(path);
-    } else {
-      let extension = path.extname(path);
-      if (extension === '.md') {
-        getMDContent(path);
-      }
-    }
+    pathAbsolute = path;
   } else {
-    const newPath = transformToAbsPath(path);
-    if (recognizeIfIsFile(newPath) === false) {
-      getFiles(path);
-    } else {
-      let extension = path.extname(path);
-    };
+    pathAbsolute = transformToAbsPath(path);
   };
+
+  if (recognizeIfIsFile(pathAbsolute) === false) {
+    getFiles(pathAbsolute).forEach((archivo) => {
+      getMDContent(archivo);
+    });
+  } else {
+    let extension = paths.extname(pathAbsolute);
+    if (extension === '.md') {
+      const content = getMDContent(pathAbsolute);
+      const contenthtml = convertMDToHtml(content);
+      const link = extractATagAttr(contenthtml);
+      arrLink = createArrLinkObj(link);
+    }
+  }
+  return arrLink;
 };
+
+console.log(mdLinks('C:/Users/Henry/Documents/javascript-proyecto-laboratoria/PROYECTO-PORTAFOLIO/LIM008-fe-md-links/TEST/carpetapadre/carpetahijo2/readme.md'));

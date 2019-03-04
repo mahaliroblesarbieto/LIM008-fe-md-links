569624cc2e1ad1c8d9fc4b210208f602b5899a19
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-03-03 19:31:10 -0500

    funcion stats con caso en el que se ingrese array con objetoscon propiedades stats y value/stats.js/stats.spec.js

--- SRC/Models/stats.js
+++ SRC/Models/stats.js
@@ -1,15 +1,26 @@
 export const calculateStats = (arrAttLinks) => {
-  const arrTotal = arrAttLinks.length;
-  const objUnique = {};
-  arrAttLinks.forEach(element => {
-    if (objUnique[element.href]) {
-      objUnique[element.href] += 1;
-    } else {
-      objUnique[element.href] = 1;
-    }
-  });
-  const objUniqueTotal = (Object.keys(objUnique)).length;
-  console.log(objUnique);
-  const stats = {total: arrTotal, unique: objUniqueTotal};
-  return stats;
+  if (arrAttLinks[0].status) {
+    console.log('entra');
+    const arrTotal = arrAttLinks.length;
+    let arrHref = [];
+    arrAttLinks.forEach(element => {
+      arrHref.push(element.href);
+    });
+    let arrUnique = arrHref.filter((valor, indiceActual, arreglo) => arreglo.indexOf(valor) === indiceActual);
+    const uniqueLength = arrUnique.length;
+    const brokenTotal = (arrAttLinks.filter(compare => (compare.value === 'Fail'))).length;
+    console.log(brokenTotal);
+    const statsValidate = {total: arrTotal, unique: uniqueLength, broken: brokenTotal};
+    return statsValidate;
+  } else {
+    const arrTotal = arrAttLinks.length;
+    let arrHref = [];
+    arrAttLinks.forEach(element => {
+      arrHref.push(element.href);
+    });
+    let arrUnique = arrHref.filter((valor, indiceActual, arreglo) => arreglo.indexOf(valor) === indiceActual);
+    const uniqueLength = arrUnique.length;
+    const stats = {total: arrTotal, unique: uniqueLength};
+    return stats;
+  }
 };
--- TEST/stats.spec.js
+++ TEST/stats.spec.js
@@ -4,13 +4,22 @@ const input = [{ href: 'https://www.npmjs.com/package/link-check', text: 'Hola M
   {href: 'https://github.com/Laboratoria/LIM008-fe-md-links/src', text: 'Hola Mundo', file: 'C:/Users/Henry/Documents/javascript-proyecto-laboratoria/PROYECTO-PORTAFOLIO/LIM008-fe-md-links/TEST/carpetapadre/carpetahijo2'},
   {href: 'https://github.com/Laboratoria/LIM008-fe-md-links/src', text: 'Hola Mundo', file: 'C:/Users/Henry/Documents/Proyecto'},
   {href: 'https://www.npmjs.com/package/link-check', text: 'Hola Mundo', file: 'C:/Users/Henry/Documents/javascript-proyecto-laboratoria/PROYECTO-PORTAFOLIO/LIM008-fe-md-links/TEST/carpetapadre/carpetahijo1'}];
+const input2 = [{ href: 'https://www.npmjs.com/package/link-check', text: 'Hola Mundo', file: 'C:/Users/Henry/Documents/javascript-proyecto-laboratoria/PROYECTO-PORTAFOLIO/LIM008-fe-md-links/TEST/carpetapadre/carpetahijo2', status: 200, value: 'OK'},
+  {href: 'https://github.com/Laboratoria/LIM008-fe-md-links/src', text: 'Hola Mundo', file: 'C:/Users/Henry/Documents/javascript-proyecto-laboratoria/PROYECTO-PORTAFOLIO/LIM008-fe-md-links/TEST/carpetapadre/carpetahijo2', status: 404, value: 'Fail'},
+  {href: 'https://github.com/Laboratoria/LIM008-fe-md-links/src', text: 'Hola Mundo', file: 'C:/Users/Henry/Documents/Proyecto', status: 404, value: 'Fail'},
+  {href: 'https://www.npmjs.com/package/link-check', text: 'Hola Mundo', file: 'C:/Users/Henry/Documents/javascript-proyecto-laboratoria/PROYECTO-PORTAFOLIO/LIM008-fe-md-links/TEST/carpetapadre/carpetahijo1', status: 200, value: 'OK'}];
 
 const ouput = {total: 4, unique: 2};
+const ouput2 = {total: 4, unique: 2, broken: 2};
+
 describe('calculateStats', () => {
   it('debería ser una función', () => {
     expect(typeof calculateStats).toBe('function');
   });
   it('Debería retornar el total de links y cuantos de los links no se repiten', () => {
     expect(calculateStats(input)).toEqual(ouput);
+  });
+  it('Debería retornar el total de links, cuantos de los links no se repiten y cuantos links son broken', () => {
+    expect(calculateStats(input2)).toEqual(ouput2);
   });
 });

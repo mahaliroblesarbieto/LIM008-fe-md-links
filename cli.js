f1b5e30775c9f194ba3c4deb15825728f0e61707
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-03-07 07:08:41 -0500

    funcion md-links permite ingresar ruta, validar y sacar estadisticas excepto validar y sacar estadisticas a la vez

--- cli.js
+++ cli.js
@@ -1,44 +1,31 @@
 #!/usr/bin/env node
 const mdLinks = require('./lib/index.js');
-const [,, ... parameters] = process.argv;
-const [path, ...opts] = parameters;
 
-const options = {
-  validate: false,
-  stats: false,
-};
+const path = process.argv[2];
 
-opts.forEach((option) => {
-  if (option === '--validate') {
-    options.validate = true;
-  }
-  if (option === '--stats') {
-    options.stats = true;
-  }
-});
-
-if (options === undefined) {
-  mdLinks(path, options)
+if (process.argv.length === 3) {
+  mdLinks(path)
     .then(response1 => {
       response1.forEach(element => {
         console.log(`${element.file}, ${element.href}, ${element.text} `);
       });
     });
-} else if (options.validate && !options.stats) {
-  mdLinks(path, options)
+} else if (process.argv.includes('--validate')) {
+  mdLinks(path, {validate: true, stats: false})
     .then(response1 => {
       response1.forEach(element => {
         console.log(`${element.file}, ${element.href}, ${element.text}, ${element.status}, ${element.value} `);
       });
     });
-} else if (!options.validate && options.stats) {
-  mdLinks(path, options)
+} else if (process.argv.includes('--stats')) {
+  mdLinks(path, {validate: false, stats: true})
     .then(response1 => {
       console.log(`Total:${response1.total} \nUnique:${response1.unique}`);
     });
-} else if (options.validate && options.stats) {
-  mdLinks(ruta, options)
+} else if (process.argv.includes('--validate') && process.argv.includes('--stats')) {
+  mdLinks(path, {validate: true, stats: true})
     .then(response1 => {
       console.log(`Total:${response1.total} \nUnique:${response1.unique} \nBroken:${response1.broken}`);
     });
-}
+};
+

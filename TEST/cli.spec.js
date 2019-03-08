fa22fae7badd0a8a2ce21abc83dbb599ec8db47d
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-03-08 07:35:14 -0500

    implementacion de funcion showInCli/Cli.js

---
+++ TEST/cli.spec.js
@@ -0,0 +1,1 @@
+const dddd = require('../cli.js');
--- cli.js
+++ cli.js
@@ -1,31 +1,34 @@
 #!/usr/bin/env node
 const mdLinks = require('./lib/index.js');
 
-const path = process.argv[2];
-
-if (process.argv.length === 3) {
-  mdLinks(path)
-    .then(response1 => {
-      response1.forEach(element => {
-        console.log(`${element.file}, ${element.href}, ${element.text} `);
+const showInCli = (argv) => {
+  if (argv.length === 3) {
+    mdLinks(argv[2])
+      .then(response1 => {
+        response1.forEach(element => {
+          console.log(`${element.file}, ${element.href}, ${element.text}`);
+        });
       });
-    });
-} else if (process.argv.includes('--validate')) {
-  mdLinks(path, {validate: true, stats: false})
-    .then(response1 => {
-      response1.forEach(element => {
-        console.log(`${element.file}, ${element.href}, ${element.text}, ${element.status}, ${element.value} `);
+  } else if (argv.includes('--validate') && argv.includes('--stats')) {
+    mdLinks(argv[2], {validate: true, stats: true})
+      .then(response1 => {
+        console.log(`Total:${response1.total} \nUnique:${response1.unique} \nBroken:${response1.broken}`);
       });
-    });
-} else if (process.argv.includes('--stats')) {
-  mdLinks(path, {validate: false, stats: true})
-    .then(response1 => {
-      console.log(`Total:${response1.total} \nUnique:${response1.unique}`);
-    });
-} else if (process.argv.includes('--validate') && process.argv.includes('--stats')) {
-  mdLinks(path, {validate: true, stats: true})
-    .then(response1 => {
-      console.log(`Total:${response1.total} \nUnique:${response1.unique} \nBroken:${response1.broken}`);
-    });
+  } else if (argv.includes('--validate')) {
+    mdLinks(argv[2], {validate: true, stats: false})
+      .then(response1 => {
+        response1.forEach(element => {
+          console.log(`${element.file}, ${element.href}, ${element.text}, ${element.status}, ${element.value}`);
+        });
+      });
+  } else if (argv.includes('--stats')) {
+    mdLinks(argv[2], {validate: false, stats: true})
+      .then(response1 => {
+        console.log(`Total:${response1.total} \nUnique:${response1.unique}`);
+      });
+  }; 
 };
 
+showInCli(process.argv);
+
+module.exports = {showInCli};
